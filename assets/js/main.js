820694f2977103d7c10d9984cf9253fa677d285b
MaTtiaa01 <mattia2001@live.it>
2022-10-06 15:45:02 +0200

    add ticket const

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-alert("it works");
+// Istruzioni
+// Il programma dovrà chiedere all'utente:
+
+
+//  il numero di chilometri che vuole percorrere
+
+const kmChoise = Number(prompt("Quati km vuoi percorrere?"));
+
+// chiedere l'età del passeggero.
+
+const passengerAge = Number(prompt("Quanti anni hai?"));
+
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+const ticketPriceForKm = 0.21
+
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+
+const baseTicketPrice = ticketPriceForKm * kmChoise
+console.log(baseTicketPrice);
